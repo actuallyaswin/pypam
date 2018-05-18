a8223a6cdb914c7287bf470b87af561ecfab9aff
Aswin Sivaraman <aswin.sivaraman@gmail.com>
2018-05-18 17:01:03 -0500

    pushing the latest model file

--- model.py
+++ model.py
@@ -3,7 +3,7 @@
 # @Author: Aswin Sivaraman
 # @Email: aswin.sivaraman@gmail.com
 # @Last Modified by:   Aswin Sivaraman
-# @Last Modified time: 2018-02-14 16:52:46
+# @Last Modified time: 2018-02-26 01:51:00
 
 __description__ = 'Applies a psychoacoustic model to a WAV file.'
 
@@ -16,15 +16,24 @@ from barkscale import *
 from tqdm import tqdm
 from scipy.io.wavfile import read
 from scipy.stats import gmean
+from os.path import basename, splitext
 
-def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
+timestamp = time.strftime('%Y%m%d_%H%M%S',time.gmtime())
+
+def MPEG1(filepath,fft_size,overlap,zeropad=False,savedata=False,saveplot=False,showplot=False,usemax=False,step=0):
     "Perceptual Model: ISO 11172-3 (MPEG-1) Psychoacoustic Model 1"
 
+    plot = saveplot or showplot
+    filename = splitext(basename(filepath))[0]
+
     # Load in the input file and normalize it
-    sample_rate, data = read(filepath)
+    # sample_rate, data = read(filepath)
+    sample_rate = 16000
+    data = np.load('sig_clean_test.csv.npy')
     data = np.divide(data, max(abs(data.min()), abs(data.max())))
-    # data = np.append(np.zeros([1,fft_size]), data)
-    # data = np.append(data, np.zeros([1,fft_size*2-(len(data)%fft_size)]))
+    if zeropad:
+        data = np.append(np.zeros([1,fft_size]), data)
+        data = np.append(data, np.zeros([1,fft_size*2-(len(data)%fft_size)]))
     window = np.hanning(fft_size)
     hop = floor(fft_size * (1-overlap))
     frames = 1 + floor((len(data)-fft_size)/hop)
@@ -32,10 +41,11 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
     # ======================================================= #
     ## Step 1 — Spectral Analysis and SPL Normalization
     # ======================================================= #
+    num_bins = fft_size//2+1
 
     if step > 0:
-        Y = np.load(filepath[:-4]+'-Y.npy')
-        P = np.load(filepath[:-4]+'-P.npy')
+        Y = np.load('output/'+filename+'-Y.npy')
+        P = np.load('output/'+filename+'-P.npy')
     else:
         # Create a DFT matrix (F)
         i, j = np.meshgrid(np.arange(fft_size), np.arange(fft_size))
@@ -51,31 +61,30 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
 
         # Apply the DFT matrix (F) to the data matrix (X) to get the STFT matrix (Y)
         Y = np.dot(X,F).T
-        if save:
-            np.save(filepath[:-4]+'-Y.npy', Y)
+        if savedata:
+            np.save('output/'+filename+'-Y.npy', Y)
 
         # Estimate the power spectral density (P)
         PN = 96 # Power normalization term
         P = 10*ma.log10(np.square(np.abs(Y))).filled(10**-10)
         P += (PN - P.max())
-        if save:
-            np.save(filepath[:-4]+'-P.npy', P)
+        if savedata:
+            np.save('output/'+filename+'-P.npy', P)
         print(">> Estimated the power spectral density")
 
     # ======================================================= #
     ## Step 2 — Identification of Tonal and Noise Maskers
     # ======================================================= #
 
-    num_bins = P.shape[0]//2+1
-    num_frames = P.shape[1]
     num_criticalbands = hz2z(sample_rate//2)-1
-
-    plot_specific_frame = 100
+    num_frames = P.shape[1]
 
     if step > 1:
-        S_T = np.load(filepath[:-4]+'-S_T.npy')
-        P_TM = np.load(filepath[:-4]+'-P_TM.npy')
-        P_NM = np.load(filepath[:-4]+'-P_NM.npy')
+        S_T = np.load('output/'+filename+'-S_T.npy')
+        S_T = np.load('output/'+filename+'-S_T.npy')
+        S_TE = np.load('output/'+filename+'-S_TE.npy')
+        P_TM = np.load('output/'+filename+'-P_TM.npy')
+        P_NM = np.load('output/'+filename+'-P_NM.npy')
     else:
         # Define tonal neighborhoods (PAINTER, Eq. 22)
         neighborhoods = RangeDict({
@@ -83,18 +92,18 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
             range(352,512): 3
             # range(int(127),int(250)): 6
         })
-
         # Find the tonal set (S_T) (PAINTER, Eq. 21)
         # Compute the tonal maskers (P_TM) (PAINTER, Eq. 23)
         # Compute the noise makers for each critical band (P_NM) (PAINTER, Eq. 24)
         S_T = np.zeros_like(P) != 0
         S_TE = np.zeros_like(P) != 0
+        S_N = np.zeros_like(P) != 0
         P_TM = np.zeros_like(P)
         P_NM = np.zeros_like(P)
         pbar = tqdm(total=(num_frames),unit='frames')
         for f in range(num_frames):
             pbar.update()
-            for k in range(2,512):
+            for k in range(2,num_bins-3):
                 _tonal = P[k-1,f] < P[k,f] > P[k+1,f]
                 if _tonal:
                     for n in range(2,neighborhoods[k]+1):
@@ -109,32 +118,24 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                             np.power(10, 0.1*P[k,f]) +
                             np.power(10, 0.1*P[k+1,f])
                         )
-            _P = P * (S_TE*-1 + 1)
+            S_N = (S_TE[:,f]*-1 + 1)
+            _P = P[:,f] * S_N
             for z in range(1,num_criticalbands+1):
                 _l = hz2bin(z2hz(z, "lower"), sr=sample_rate, fft_size=fft_size)
                 _u = hz2bin(z2hz(z, "upper"), sr=sample_rate, fft_size=fft_size)
                 _kbar = int(gmean(range(_l,_u))) # (PAINTER, Eq. 25)
-                # _j = []
-                # for j in range(_l,_u+1):
-                #     _nontonal = not (S_T[j,f] or S_T[j-1,f] or S_T[j+1,f])
-                #     if neighborhoods[_kbar]:
-                #         for n in range(2,neighborhoods[_kbar]+1):
-                #             _nontonal = _nontonal and not (S_T[j-n,f] or S_T[j+n,f])
-                #         if _nontonal:
-                #             _j.append(j)
-                # P_NM[_kbar,f] = 10 * np.log10(1 + sum([np.power(10, 0.1*_P[j,f]) for j in _j]))
-                P_NM[_kbar,f] = 10 * np.log10(sum(np.power(10, 0.1*_P[_l:_u,f])))
+                P_NM[_kbar,f] = 10 * np.log10(sum(np.power(10, 0.1*_P[_l:_u])))
         pbar.close()
-        if save:
-            np.save(filepath[:-4]+'-S_T.npy', S_T[:num_bins])
-            np.save(filepath[:-4]+'-S_TE.npy', S_TE[:num_bins])
-            np.save(filepath[:-4]+'-P_TM.npy', P_TM[:num_bins])
-            np.save(filepath[:-4]+'-P_NM.npy', P_NM[:num_bins])
+        if savedata:
+            np.save('output/'+filename+'-S_T.npy', S_T[:num_bins])
+            np.save('output/'+filename+'-S_TE.npy', S_TE[:num_bins])
+            np.save('output/'+filename+'-P_TM.npy', P_TM[:num_bins])
+            np.save('output/'+filename+'-P_NM.npy', P_NM[:num_bins])
         print(">> Found tonal set and tonal neighbors")
         print(">> Computed tonal maskers")
         print(">> Computed noise makers for each critical band")
 
-    if 0: #plot:
+    if plot:
         fig, axes = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(24,9), dpi=100)
         axes[0,0].imshow(abs(Y[:num_bins]),origin='lower', aspect='auto')
         axes[0,1].imshow(abs(P[:num_bins]),origin='lower', aspect='auto')
@@ -154,10 +155,12 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
         axes[1,0].axis('off')
         axes[1,1].axis('off')
         axes[1,2].axis('off')
-        # fig.text(0.5, 0.005, 'Time Frame [f]', ha='center')
-        # fig.text(0.005, 0.5, 'Frequency Bins [k]', va='center', rotation='vertical')
+        fig.text(0.5, 0.005, 'Time Frame [f]', ha='center')
+        fig.text(0.005, 0.5, 'Frequency Bins [k]', va='center', rotation='vertical')
         plt.tight_layout()
-        plt.savefig('plt_step2_'+str(time.time())+'.png')
+    if saveplot:
+        plt.savefig('plots/'+timestamp+'_'+filename+'_STEP2', format='eps')
+    if showplot:
         plt.show()
 
     # ======================================================= #
@@ -165,22 +168,22 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
     # ======================================================= #
 
     if step > 2:
-        T_q = np.load(filepath[:-4]+'-T_q.npy')
+        T_q = np.load('output/'+filename+'-T_q.npy')
     else:
 
         oldP_TM = P_TM.copy()
         oldP_NM = P_NM.copy()
 
-        print(np.sum(oldP_NM > 0) + np.sum(oldP_TM > 0))
+        print(">> Number of maskers before decimation =",np.sum(oldP_NM > 0) + np.sum(oldP_TM > 0))
 
         _f, _step = np.linspace(0, sample_rate, fft_size, endpoint=False, retstep=True)
         _f = np.add(_f, _step/2)
         T_q = (3.64 * np.power(_f/1000, -0.8)) \
             - (6.5 * np.exp(-0.6*(np.power((_f/1000)-3.3, 2)))) \
             + (0.001 * np.power(_f/1000, 4))
-        assert T_q.shape[0] == P.shape[0]
-        if save:
-            np.save(filepath[:-4]+'-T_q.npy', T_q[:num_bins])
+
+        if savedata:
+            np.save('output/'+filename+'-T_q.npy', T_q)
         print(">> Constructed absolute threshold")
 
         if plot:
@@ -196,21 +199,26 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
             axes[0].legend(handles=[l[0],l[1],l[2],l[3]])
             axes[0].set_title(r'Before decimation')
 
+        pbar = tqdm(total=(num_frames),unit='frames')
         for f in range(num_frames):
+            pbar.update()
             for k in range(num_bins):
                 if P_TM[k,f] < T_q[k]:
                     S_T[k,f] = False
                     P_TM[k,f] = 0
                 if P_NM[k,f] < T_q[k]:
                     P_NM[k,f] = 0
+        pbar.close()
         print(">> Discarded tonal or noise makers below the absolute threshold")
 
+        pbar = tqdm(total=(num_frames),unit='frames')
         for f in range(num_frames):
+            pbar.update()
             _zw = 0.5
             _zs = 0.25
             for z in np.arange(1,num_criticalbands,_zs):
                 _bl = z2bin(z-_zw, sr=sample_rate, fft_size=fft_size, traunmuller=True)
-                _bh = z2bin(z+_zw, sr=sample_rate, fft_size=fft_size, traunmuller=True)
+                _bh = min(num_bins, z2bin(z+_zw, sr=sample_rate, fft_size=fft_size, traunmuller=True))
                 _localmax = max(P_TM[_bl:_bh,f].max(), P_NM[_bl:_bh,f].max())
                 if _localmax > 0:
                     for k in range(_bl,_bh):
@@ -219,11 +227,14 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                             P_TM[k,f] = 0
                         if P_NM[k,f] < _localmax:
                             P_NM[k,f] = 0
+        pbar.close()
         print(">> Consolidated maskers that are within +/- 0.5 barks")
 
         _T = np.zeros_like(P_TM)
         _N = np.zeros_like(P_TM)
+        pbar = tqdm(total=(num_frames),unit='frames')
         for f in range(num_frames):
+            pbar.update()
             for k in range(num_bins):
                 if 1 <= k <= 264:
                     i = k
@@ -239,7 +250,14 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                     P_NM[k,f] = 0
         P_TM = _T
         P_NM = _N
+        pbar.close()
         print(">> Subsampled maskers")
+
+        print(">> Number of maskers after decimation =",np.sum(P_NM > 0) + np.sum(P_TM > 0))
+
+        if savedata:
+            np.save('output/'+filename+'-P_TM.npy', P_TM[:num_bins])
+            np.save('output/'+filename+'-P_NM.npy', P_NM[:num_bins])
 
         if plot:
             l[4], = axes[1].plot(_pz,P[:num_bins,0])
@@ -261,6 +279,9 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                 l[6].set_ydata(P_TM[:num_bins,frame])
                 l[7].set_ydata(P_NM[:num_bins,frame])
             sframe.on_changed(update)
+        if saveplot:
+            plt.savefig('plots/'+timestamp+'_'+filename+'_STEP3', format='eps')
+        if showplot:
             plt.show()
 
     # ======================================================= #
@@ -268,8 +289,8 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
     # ======================================================= #
 
     if step > 3:
-        T_TM = np.load(filepath[:-4]+'-T_TM.npy')
-        T_NM = np.load(filepath[:-4]+'-T_NM.npy')
+        T_TM = np.load('output/'+filename+'-T_TM.npy')
+        T_NM = np.load('output/'+filename+'-T_NM.npy')
     else:
         # Compute individual threshold contributions from each tonal
         # and noise masker (PAINTER, Eq. 30/31/32)
@@ -301,25 +322,24 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                             _SFTM = (0.15*P_TM[j,f] - 17)*_dz - (0.15*P_TM[j,f])
                             _SFNM = (0.15*P_NM[j,f] - 17)*_dz - (0.15*P_NM[j,f])
                         # print('masker j={}, maskee i={}, masker zj={}, maskee zi={}, dz={}, SF={}'.format(j,i,_zj,_zi,_dz,_SFTM))
-                        T_TM[i,f] += P_TM[j,f] - 0.275*_zj + _SFTM - 6.025
-                        T_NM[i,f] += P_NM[j,f] - 0.175*_zj + _SFNM - 2.025
-            plt.plot(P_NM[:,f])
-            plt.show()
+                        if usemax:
+                            T_TM[i,f] = max(T_TM[i,f], P_TM[j,f] - 0.275*_zj + _SFTM - 6.025)
+                            T_NM[i,f] = max(T_NM[i,f], P_NM[j,f] - 0.175*_zj + _SFNM - 2.025)
+                        else:
+                            T_TM[i,f] += P_TM[j,f] - 0.275*_zj + _SFTM - 6.025
+                            T_NM[i,f] += P_NM[j,f] - 0.175*_zj + _SFNM - 2.025
         pbar.close()
-        if save:
-            np.save(filepath[:-4]+'-T_TM.npy', T_TM[:num_bins])
-            np.save(filepath[:-4]+'-T_NM.npy', T_NM[:num_bins])
+        if savedata:
+            np.save('output/'+filename+'-T_TM.npy', T_TM[:num_bins])
+            np.save('output/'+filename+'-T_NM.npy', T_NM[:num_bins])
         print(">> Computed individual tonal & noise masker thresholds")
-
-    print(T_TM.max())
-    print(T_NM.max())
 
     # ======================================================= #
     ## Step 5 — Calculation of Global Masking Thresholds
     # ======================================================= #
 
     if step > 4:
-        T_g = np.load(filepath[:-4]+'-T_g.npy')
+        T_g = np.load('output/'+filename+'-T_g.npy')
     else:
         # Combine individual masking thresholds to create a global
         # masking threshold at each frequency bin (PAINTER, Eq. 33)
@@ -334,8 +354,8 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
                         np.power(10, 0.1*T_NM[k,f])
                     ]))
         pbar.close()
-        if save:
-            np.save(filepath[:-4]+'-T_g.npy', T_g[:num_bins])
+        if savedata:
+            np.save('output/'+filename+'-T_g.npy', T_g[:num_bins])
         print(">> Computed global masking threshold")
 
     # ======================================================= #
@@ -343,17 +363,42 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
     # ======================================================= #
 
     if step > 5:
-        W = np.load(filepath[:-4]+'-W.npy')
+        W = np.load('output/'+filename+'-W.npy')
     else:
         # Generate psychoacoustic weights
         W = np.log10( np.power(10, 0.1*P) / np.power(10, 0.1*T_g) + 1)
-        if save:
-            np.save(filepath[:-4]+'-W.npy', W[:num_bins])
+        if savedata:
+            np.save('output/'+filename+'-W.npy', W[:num_bins])
         print(">> Generated psychoacoustic weights")
-        print('-- Saved results to '+filepath[:-4]+'-W.npy, of shape: '+str(W.shape)+' min: '+str(W.min())+' max: '+str(W.max()))
+        print('-- Saved results to '+'output/'+filename+'-W.npy, of shape: '+str(W[:num_bins].shape)+' min: '+str(W.min())+' max: '+str(W.max()))
 
     if plot:
-        fig, axes = plt.subplots(1, 3, sharey=True, figsize=(12,4), dpi=100)
+        fig, axes = plt.subplots(2, 1, sharex=True)
+        l1, = axes[0].plot(P[:num_bins,0])
+        l2, = axes[0].plot(T_g[:num_bins,0])
+        axes[0].legend([l1,l2],[r'Power Spectral Density, $P$',r'Global Masking Threshold, $T_g$'])
+        axes[0].set_xlabel('Frequency Bin')
+        axes[0].set_ylabel('dB-SPL')
+        l3, = axes[1].plot(W[:num_bins,0])
+        axes[1].set_ylim([0, 10])
+        axes[1].set_ylabel('Perceptual Weight Values')
+        axes[1].set_xlabel('Frequency Bin')
+    if saveplot:
+        plt.savefig('plots/'+timestamp+'_'+filename+'_STEP6_1', format='eps')
+    if showplot:
+        # Slider parameters
+        axcolor = 'lightgoldenrodyellow'
+        axframe = plt.axes([0.2, 0.05, 0.6, 0.01], facecolor=axcolor)
+        sframe = Slider(axframe, 'Frame', 0, num_frames-1, valinit=0, valfmt='%0.0f')
+        def update(val):
+            frame = int(np.around(sframe.val))
+            l1.set_ydata(P[:num_bins,frame])
+            l2.set_ydata(T_g[:num_bins,frame])
+            l3.set_ydata(W[:num_bins,frame])
+        sframe.on_changed(update)
+        plt.show()
+    if plot:
+        fig, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(12,4), dpi=100)
         fig.colorbar( axes[0].imshow(P[:num_bins], origin="lower", aspect='auto'), ax=axes[0] )
         fig.colorbar( axes[1].imshow(T_g[:num_bins], origin="lower", aspect='auto'), ax=axes[1] )
         fig.colorbar( axes[2].imshow(W[:num_bins], origin="lower", aspect='auto'), ax=axes[2] )
@@ -366,7 +411,9 @@ def MPEG1(filepath, fft_size, overlap, save=False, plot=False, step=0):
         fig.text(0.5, 0.005, 'Time Frame [f]', ha='center')
         fig.text(0.005, 0.5, 'Frequency Bins [k]', va='center', rotation='vertical')
         plt.tight_layout()
-        plt.savefig('plt_step6_2_'+str(time.time())+'.png')
+    if saveplot:
+        plt.savefig('plots/'+timestamp+'_'+filename+'_STEP6_2', format='eps')
+    if showplot:
         plt.show()
 
     return
@@ -381,10 +428,16 @@ if __name__ == "__main__":
                         help='number of frequency bins (default: 1024)')
     parser.add_argument('--fft_overlap', metavar='', type=float, default=0.75,
                         help='percentage of overlap between frames (default: 0.75)')
-    parser.add_argument('--save', action='store_true',
+    parser.add_argument('--savedata', action='store_true',
                         help='in order to save intermediate values')
-    parser.add_argument('--plot', action='store_true',
-                        help='in order to plot model')
+    parser.add_argument('--usemax', action='store_true',
+                        help='to overwrite individual threshold components with just the max')
+    parser.add_argument('--saveplot', action='store_true',
+                        help='in order to save the plot model')
+    parser.add_argument('--showplot', action='store_true',
+                        help='in order to show the plot model')
+    parser.add_argument('--zeropad', action='store_true',
+                        help='in order to zeropad the input')
     parser.add_argument('--step', metavar='', type=int, default=0,
                         help='in order to skip to a particular step')
 
@@ -394,7 +447,7 @@ if __name__ == "__main__":
         print('Unable to locate model {}, now using MPEG1...'.format(args.model))
         args.model = 'MPEG1'
 
-    if args.plot:
+    if args.saveplot or args.showplot:
         import matplotlib.pyplot as plt
         from matplotlib.widgets import Slider
         from mpl_toolkits.mplot3d import Axes3D
@@ -407,8 +460,11 @@ if __name__ == "__main__":
         filepath=args.filepath,
         fft_size=args.fft_size,
         overlap=args.fft_overlap,
-        save=args.save,
-        plot=args.plot,
+        zeropad=args.zeropad,
+        savedata=args.savedata,
+        saveplot=args.saveplot,
+        showplot=args.showplot,
+        usemax=args.usemax,
         step=args.step
     )
 
